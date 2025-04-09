26594f1f00c6326b3ac27d3324ee072c053f94f6
Ivan Stakhov <50211739+left-try@users.noreply.github.com>
2025-04-09 21:19:51 +0600

    Test

--- stand/liblua/lutils.c
+++ stand/liblua/lutils.c
@@ -389,6 +389,10 @@ lua_writefile(lua_State *L)
 #include "efi.h"
 #include "efilib.h"
 
+extern EFI_SYSTEM_TABLE		*ST = NULL;
+extern EFI_BOOT_SERVICES	*BS;
+extern EFI_RUNTIME_SERVICES	*RS;
+
 static int
 lua_efi_get_vendor(lua_State *L)
 {
